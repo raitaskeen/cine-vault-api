ce4355703f2ebb0fc8db6b0ece2a149a51a3d801
Taskeen Haider <raitaskeenhaider786@gmail.com>
2026-07-28 23:00:39 +0500

    feat: added new features in CRUD (Update Function into the movies.controller.js and in movies.routes.js also added the logic)

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -17,3 +17,29 @@ exports.getAllMovies = async (req, res) => {
         res.status(500).json({ success: false, error: "Server Error" });
     }
 };
+
+exports.updateMovie = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const newData = req.body;
+
+        const updatedMovie = await Movie.findByIdAndUpdate(id, newData, { 
+        new: true,
+        runValidators: true
+        });
+    if (!updatedMovie) {
+        return res.status(404).json({ 
+            success: false, error: "Movie not found" });
+        }
+    res.status(200).json({ 
+        success: true, 
+        data: updatedMovie 
+    });
+} catch (error) {
+    res.status(400).json({
+        success: false,
+        error: error.message
+    });
+}
+};
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,6 +4,9 @@ const controller = require("../controllers/movies.controller");
 const router = express.Router();
 
 router.post("/", controller.createMovie);
+
 router.get("/", controller.getAllMovies);
+
+router.put("/:id", controller.updateMovie);
 
 module.exports = router;
